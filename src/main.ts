46c246ddf40f77760fdafd20e77497aa469997af
COCOLGH <1772527953@qq.com>
2024-02-27 18:25:27 +0800

    learn record

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,15 @@
-import { createApp } from "vue";
-import App from "./App.vue";
+import { createApp, createVNode, defineComponent, h } from "vue"
+// import App from "./App.vue"
+import HelloWorld from "./components/HelloWorld.vue"
+const img = require("./assets/logo.png") // eslint-disable-line
 
-createApp(App).mount("#app");
+const App = defineComponent({
+  render() {
+    return createVNode("div", { id: "app" }, [
+      createVNode("img", { alt: "vue logo", src: img }),
+      createVNode(HelloWorld, { msg: "测试" }),
+    ])
+  },
+})
+
+createApp(App).mount("#app")

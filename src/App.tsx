9b6558828646eb7cb9086c58e8295c641d7b252c
cocol <cocol@qq.com>
2024-03-06 22:55:34 +0800

    learn record

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
-import { defineComponent, h, ref, reactive } from "vue"
-const img = require("./assets/logo.png") // eslint-disable-line
+/*
+ * @Author: cocol cocol@qq.com
+ * @Date: 2024-03-04 23:36:42
+ * @LastEditors: cocol cocol@qq.com
+ * @LastEditTime: 2024-03-06 22:51:59
+ * @FilePath: \vue3\src\App.tsx
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+import { defineComponent, ref, reactive } from "vue"
 
 export default defineComponent({
   setup() {
@@ -18,7 +25,6 @@ export default defineComponent({
       const num = numberRef.value
       return (
         <div id="app">
-          <img src={img} alt="vue logo.png" />
           <p>{state.name + num}</p>
         </div>
       )
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,11 @@
+/*
+ * @Author: cocol cocol@qq.com
+ * @Date: 2024-03-04 23:36:42
+ * @LastEditors: cocol cocol@qq.com
+ * @LastEditTime: 2024-03-05 00:13:52
+ * @FilePath: \vue3\src\main.ts
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 import { createApp } from "vue"
 import App from "./App"
 
